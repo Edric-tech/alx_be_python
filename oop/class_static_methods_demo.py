5112c6a87e9d9c963b3e3175b14dc6f64d1bedb0
Edric Bunei <buneiedric03@gmail.com>
2025-10-12 13:47:51 +0300

    OOP projects added

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,12 @@
+class Calculator:
+    calculation_type = "Arithmetic Operations"
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @classmethod
+    def multiply(cls, a, b):
+        """Class method to calculate the product of two numbers and print the calculation type."""
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
